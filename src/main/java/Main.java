f0046c63b6a388076a5b689c365b1293ef9fca6a
Deian Golubov <deian.golubov@yahoo.com>
2021-05-08 20:54:29 +0300

    Created register form

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-package org.fis.student;
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println("first main commit");
-    }
-}
---
+++ src/main/java/org/loose/tyb/Main.java
@@ -0,0 +1,37 @@
+package org.loose.tyb;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.loose.tyb.services.FileSystemService;
+import org.loose.tyb.services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        initDirectory();
+        UserService.initDatabase();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("register.fxml")));
+        primaryStage.setTitle("Trade Your Books");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    private void initDirectory() {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
